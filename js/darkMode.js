49df562ed08713868386b47ecb1a1b6918b86292
minecraftjmh30-boop <minecraftjmh30@gmail.com>
2026-08-20 20:38:08 -0500

    toggle dark mode now changes text

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,21 +1,26 @@
 const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const pageHTML = document.querySelector('html');
 const darkModeToggle = document.getElementById('btn-check');
+const darkModeLabel = document.querySelector('label[for="btn-check"]');
 
 document.addEventListener("DOMContentLoaded", function () {
     if (isDarkMode) {
         pageHTML.setAttribute('data-bs-theme', 'dark');
+        if (darkModeLabel) darkModeLabel.innerText = "Toggle Light Mode";
         if (darkModeToggle) darkModeToggle.checked = true;
     } else {
         pageHTML.setAttribute('data-bs-theme', 'light');
+        if (darkModeLabel) darkModeLabel.innerText = "Toggle Dark Mode";
     }
 
     if (darkModeToggle) {
         darkModeToggle.addEventListener('change', function () {
             if (this.checked) {
                 pageHTML.setAttribute('data-bs-theme', 'dark');
+                if (darkModeLabel) darkModeLabel.innerText = "Toggle Light Mode";
             } else {
                 pageHTML.setAttribute('data-bs-theme', 'light');
+                if (darkModeLabel) darkModeLabel.innerText = "Toggle Dark Mode";
             }
         });
     }
